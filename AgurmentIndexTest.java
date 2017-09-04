f859ecb26e64e9ae4fe51d4fb16b1a4a56ac6619
Kolawole Ezekiel <Doneazy911@gmail.com>
2017-09-04 09:41:40 +0100

    Add files via upload

---
+++ AgurmentIndexTest.java
@@ -0,0 +1,13 @@
+
+package agurmentindextest;
+
+public class AgurmentIndexTest {
+
+    public static void main(String[] args) {
+System.out.printf("Numbers before reordering: %d %d %d %d%n",1,2,3,4);
+System.out.printf("Numbers after ordering: %4$d %3$d %2$d %1$d%n",1,2,3,4);
+
+
+    }
+    
+}
